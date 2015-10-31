e1353c7daec1b7a25291026af7704238f90e628e
Sarah Dunn <sdunn@carthage.edu>
2015-10-31 16:55:58 -0500

    Started work on the Evaluator and EvaluatorTests classes

--- Evaluator.java
+++ Evaluator.java
@@ -1,4 +1,251 @@
+import java.util.ArrayList;
 
-public class Evaluator {
+public class Evaluator 
+{
+	private int numCards;
+	private int numHands;
+	
+	public Evaluator(int n)
+	{
+		numCards = n;
+	}
+	
+	public int getNumCards()
+	{
+		return numCards;
+	}
+	
+	public void setNumCards(int numCards)
+	{
+		this.numCards = numCards;
+	}
+	
+	public int getNumHands() 
+	{
+		return numHands;
+	}
 
+	public void setNumHands(int numHands) 
+	{
+		this.numHands = numHands;
+	}
+
+	public void playAndDisplay()
+	{
+		
+	}
+
+	public boolean isRoyalFlush(ArrayList < Card > hand)
+	{
+		return false;
+		
+	}
+	
+	public boolean isStraightFlush(ArrayList < Card > hand)
+	{
+		return false;
+		
+	}
+	
+	public boolean isFourOfAKind(ArrayList < Card > hand)
+	{
+		boolean retVal = false;
+		ArrayList < Integer > counters = new ArrayList < Integer >();
+				
+		numericValueCounter(counters, hand);
+		for(int i = 0; i < counters.size(); i++)
+		{
+			if(counters.get(i) >= 4)
+			{
+				retVal = true;
+			}
+		}
+		return retVal;
+	}
+	
+	public boolean isFullHouse(ArrayList < Card > hand)
+	{
+		//counter keeps track of number of times each numeric 
+		//value appears in the hand
+		ArrayList < Integer > counters = new ArrayList < Integer >();
+		numericValueCounter(counters, hand);
+		
+		boolean isThree = false;
+		boolean isTwo = false;
+		int threeOfAKindPos = 0;
+		
+		//checks to see if there were 3 of any card
+		for(int i = 0; i < counters.size(); i++)
+		{
+			if(counters.get(i) >= 3)
+			{
+				isThree = true;
+				threeOfAKindPos = i;
+			}
+		}
+		
+		//checks to see if there were 2 of any card
+		for(int i = 0; i < counters.size(); i++)
+		{
+			if((counters.get(i) >= 2 && i != threeOfAKindPos))
+				isTwo = true;
+		}
+		
+		return isThree && isTwo ? true : false;
+	}
+	
+	public boolean isFlush(ArrayList < Card > hand)
+	{
+		return false;
+	}
+	
+	public boolean isStraight(ArrayList < Card > hand)
+	{
+		boolean retVal = false;
+		sortHand(hand);
+		
+		if(hand.size() == 5 && hand.get(4).getNumValue() - hand.get(0).getNumValue() == 4)
+		{
+			retVal = true;
+		}
+		else if(hand.size() == 7)
+		{
+			if(hand.get(4).getNumValue() - hand.get(0).getNumValue() == 4 || 
+					hand.get(5).getNumValue() - hand.get(1).getNumValue() == 4 || 
+					hand.get(6).getNumValue() - hand.get(2).getNumValue() == 4)
+			{
+				retVal = true;
+			}
+		}
+		
+		return retVal;
+	}
+	
+	public boolean isThreeOfAKind(ArrayList < Card > hand)
+	{
+		boolean retVal = false;
+		ArrayList < Integer > counters = new ArrayList < Integer >();
+				
+		numericValueCounter(counters, hand);
+		for(int i = 0; i < counters.size(); i++)
+		{
+			if(counters.get(i) >= 3)
+			{
+				retVal = true;
+			}
+		}
+		return retVal;
+	}
+	
+	public boolean isTwoPair(ArrayList < Card > hand)
+	{
+		int numPairs = pairCounter(hand);
+		
+		return numPairs > 1 ? true : false;
+	}
+	
+	public boolean isPair(ArrayList < Card > hand)
+	{
+		int numPairs = pairCounter(hand);
+		
+		return numPairs > 0 ? true : false;
+	}
+	
+	public int pairCounter(ArrayList < Card > hand)
+	{
+		int retVal = 0;
+		
+		ArrayList < Integer > counters = new ArrayList < Integer >();
+		numericValueCounter(counters, hand);
+		
+		for(int i = 0; i < counters.size(); i++)
+		{
+			if(counters.get(i) == 2 || counters.get(i) == 3)
+			{
+				retVal++;
+			}
+			else if(counters.get(i) == 4)
+			{
+				retVal += 2;
+			}
+		}
+		
+		return retVal;
+	}
+	
+	public void numericValueCounter(ArrayList < Integer > counters, ArrayList < Card > hand)
+	{
+		for(int i = 0; i <= 14; i++)
+			counters.add(0);
+		
+		for(int i = 0; i < hand.size(); i++)
+		{
+			counters.set(hand.get(i).getNumValue(), counters.get(hand.get(i).getNumValue()) + 1);
+		}
+	}
+	
+	public void sortHand(ArrayList < Card > hand)
+	{
+		for(int i = 0; i < hand.size(); i++)
+		{
+			for(int j = i + 1; j < hand.size(); j++)
+			{
+				if(hand.get(i).getNumValue() > hand.get(j).getNumValue())
+				{
+					Card c = hand.get(i);
+					hand.set(i, hand.get(j));
+					hand.set(j, c);
+				}
+			}
+		}
+	}
+	
+	public void flushFinder(ArrayList < Card > flush, ArrayList < Card > hand)
+	{
+		int suitCounter[] = { 0, 0, 0, 0};
+		
+		//this loop goes through the hand and adds to the suit 
+		//counter each time it reaches a certain suit
+		for(int i = 0; i < hand.size(); i++)
+		{
+			if(hand.get(i).getSuitValue() == "Hearts")
+				suitCounter[0]++;
+			else if(hand.get(i).getSuitValue() == "Clubs")
+				suitCounter[1]++;
+			else if(hand.get(i).getSuitValue() == "Spades")
+				suitCounter[2]++;
+			else if(hand.get(i).getSuitValue() == "Diamonds")
+				suitCounter[3]++;
+		}
+		
+		String suit;
+
+		//determines if it is a flush and also determines what 
+		//suit the flush is made up of
+		for(int i = 0; i < 4; i++)
+		{
+			if(suitCounter[i] >= 5)
+			{
+				if(i == 0)
+					suit = "Hearts";
+				else if(i == 1)
+					suit = "Clubs";
+				else if(i == 2)
+					suit = "Spades";
+				else
+					suit = "Diamonds";
+				
+				//puts the flush cards into a new ArrayList with only those cards
+				for(int j = 0; j < hand.size(); j++)
+				{
+					if(hand.get(j).getSuitValue().equals(suit))
+					{
+						flush.add(hand.get(j));
+					}
+				}
+				
+				break;
+			}
+		}
+	}
 }
--- EvaluatorTests.java
+++ EvaluatorTests.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,7 +9,8 @@ import org.junit.Test;
 public class EvaluatorTests 
 {
 	Card c1, c2, c3, c4, c5, c6, c7;
-	
+	Evaluator e5;
+	Evaluator e7;
 	@Before
 	public void setUp() throws Exception 
 	{
@@ -18,6 +21,9 @@ public class EvaluatorTests
 		c5 = new Card(6, "Hearts");
 		c6 = new Card(7, "Hearts");
 		c7 = new Card(8, "Hearts");
+		
+		e5 = new Evaluator(5);
+		e7 = new Evaluator(7);
 	}
 
 	@After
@@ -41,13 +47,120 @@ public class EvaluatorTests
 	@Test
 	public void testFourOfAKind() 
 	{
-		fail("Not yet implemented");
+		ArrayList< Card > hand = new ArrayList < Card >();
+		
+		//2h, 3h, 4h, 5h, 6h
+		hand.add(c1);
+		hand.add(c2);
+		hand.add(c3);
+		hand.add(c4);
+		hand.add(c5);
+		assertEquals(false, e5.isFourOfAKind(hand));
+		try
+		{
+			c2.setNumValue(2);
+			c2.setSuitValue("Spades");
+			c3.setNumValue(2);
+			c3.setSuitValue("Clubs");
+			c4.setNumValue(2);
+			c4.setSuitValue("Diamonds");
+		}
+		catch (CardException e)
+		{
+			fail();
+		}
+		
+		//2h, 2s, 2c, 2d, 6h
+		assertEquals(true, e5.isFourOfAKind(hand));
+		
+		hand.add(c6);
+		hand.add(c7);
+		//2h, 2s, 2c, 2d, 6h, 7h, 8h
+		assertEquals(true, e7.isFourOfAKind(hand));
+		try
+		{
+			c2.setNumValue(3);
+		}
+		catch (CardException e)
+		{
+			fail();
+		}
+		
+		//2h, 3s, 2c, 2d, 6h, 7h, 8h
+		assertEquals(false, e7.isFourOfAKind(hand));
 	}
 	
 	@Test
 	public void testFullHouse() 
 	{
-		fail("Not yet implemented");
+		ArrayList< Card > hand = new ArrayList < Card >();
+		
+		//2h, 3h, 4h, 5h, 6h
+		hand.add(c1);
+		hand.add(c2);
+		hand.add(c3);
+		hand.add(c4);
+		hand.add(c5);
+		assertEquals(false, e5.isFullHouse(hand));
+		try
+		{
+			c2.setNumValue(2);
+			c2.setSuitValue("Clubs");
+			c3.setNumValue(3);
+			c4.setNumValue(3);
+			c4.setSuitValue("Clubs");
+			c5.setNumValue(3);
+			c5.setSuitValue("Spades");
+		}
+		catch (CardException e)
+		{
+			fail();
+		}
+		//2h, 2c, 3h, 3c, 3s
+		assertEquals(true, e5.isFullHouse(hand));
+		
+		hand.add(c6);
+		hand.add(c7);
+		//2h, 2c, 3h, 3c, 3s, 7h, 8h
+		assertEquals(true, e7.isFullHouse(hand));
+		
+		try
+		{
+			c6.setNumValue(2);
+			c6.setSuitValue("Spades");
+		}
+		catch (CardException e)
+		{
+			fail();
+		}
+		//2h, 2c, 3h, 3c, 3s, 2s, 8h
+		assertEquals(true, e7.isFullHouse(hand));
+		
+		
+		try
+		{
+			c7.setNumValue(2);
+			c7.setSuitValue("Diamonds");
+		}
+		catch (CardException e)
+		{
+			fail();
+		}
+		//2h, 2c, 3h, 3c, 3s, 2s, 2d
+		assertEquals(true, e7.isFullHouse(hand));
+		
+		try
+		{
+			c2.setNumValue(4);
+			c6.setNumValue(5);
+			c7.setNumValue(6);
+		}
+		catch (CardException e)
+		{
+			fail();
+		}
+		//2h, 4c, 3h, 3c, 3s, 5s, 6d
+		assertEquals(false, e7.isFullHouse(hand));
 	}
 	
 	@Test
@@ -59,24 +172,387 @@ public class EvaluatorTests
 	@Test
 	public void testStraight() 
 	{
-		fail("Not yet implemented");
+		ArrayList < Card > hand = new ArrayList < Card >();
+		
+		//2h, 3h, 4h, 5h, 6h
+		hand.add(c1);
+		hand.add(c2);
+		hand.add(c3);
+		hand.add(c4);
+		hand.add(c5);
+		assertEquals(true, e5.isStraight(hand));
+		
+		try
+		{
+			c2.setNumValue(12);
+		}
+		catch (CardException e)
+		{
+			fail();
+		}
+		//2h, 12h, 4h, 5h, 6h
+		assertEquals(false, e5.isStraight(hand));
+		
+		
+		hand.add(c7);
+		hand.add(c6);
+		//2h, 12h, 4h, 5h, 6h, 8h, 7h
+		assertEquals(true, e7.isStraight(hand));
+		
+		try
+		{
+			c6.setNumValue(14);
+		}
+		catch (CardException e)
+		{
+			fail();
+		}
+		//2h, 12h, 4h, 5h, 6h, 8h, 14h
+		assertEquals(false, e7.isStraight(hand));
+		
+		try
+		{
+			c2.setNumValue(3);
+			c6.setNumValue(7);
+		}
+		catch (CardException e)
+		{
+			fail();
+		}
+		
+		//2h, 3h, 4h, 5h, 6h, 8h, 7h
+		assertEquals(true, e7.isStraight(hand));
+		
 	}
 	
 	@Test
 	public void testThreeOfAKind() 
 	{
-		fail("Not yet implemented");
+		ArrayList< Card > hand = new ArrayList < Card >();
+		
+		//2h, 3h, 4h, 5h, 6h
+		hand.add(c1);
+		hand.add(c2);
+		hand.add(c3);
+		hand.add(c4);
+		hand.add(c5);
+		assertEquals(false, e5.isThreeOfAKind(hand));
+		try
+		{
+			c2.setNumValue(2);
+			c2.setSuitValue("Spades");
+			c3.setNumValue(2);
+			c3.setSuitValue("Clubs");
+		}
+		catch (CardException e)
+		{
+			fail();
+		}
+		
+		//2h, 2s, 2c, 5h, 6h
+		assertEquals(true, e5.isThreeOfAKind(hand));
+		
+		hand.add(c6);
+		hand.add(c7);
+		//2h, 2s, 2c, 5h, 6h, 7h, 8h
+		assertEquals(true, e7.isThreeOfAKind(hand));
+		try
+		{
+			c2.setNumValue(3);
+		}
+		catch (CardException e)
+		{
+			fail();
+		}
+		
+		//2h, 3s, 2c, 5h, 6h, 7h, 8h
+		assertEquals(false, e7.isThreeOfAKind(hand));
 	}
 
 	@Test
 	public void testTwoPair() 
 	{
-		fail("Not yet implemented");
+		ArrayList < Card > hand = new ArrayList < Card >();
+		
+		try 
+		{
+			c2.setNumValue(2);
+			c2.setSuitValue("Clubs");
+			
+			c3.setNumValue(3);
+			c4.setNumValue(3);
+			c4.setSuitValue("Clubs");
+		} 
+		catch (CardException e) 
+		{
+			fail();
+		}
+		
+		hand.add(c1);
+		hand.add(c2);
+		hand.add(c3);
+		hand.add(c4);
+		hand.add(c5);
+		//2h, 2c, 3h, 3c, 6h
+		assertEquals(true, e5.isTwoPair(hand));
+		
+		try 
+		{
+			c2.setNumValue(4);
+		} 
+		catch (CardException e) 
+		{
+			fail();
+		}
+		//2h, 4c, 3h, 3c, 6h
+		assertEquals(false, e5.isTwoPair(hand));
+		
+		hand.add(c6);
+		hand.add(c7);
+		//2h, 4c, 3h, 3c, 6h, 7h, 8h
+		assertEquals(false, e7.isTwoPair(hand));
+		
+		try 
+		{
+			c2.setNumValue(2);
+		} 
+		catch (CardException e) 
+		{
+			fail();
+		}
+		//2h, 2c, 3h, 3c, 6h, 7h, 8h
+		assertEquals(true, e7.isTwoPair(hand));
 	}
 	
 	@Test
 	public void testPair() 
 	{
-		fail("Not yet implemented");
+		ArrayList < Card > hand = new ArrayList < Card >();
+		
+		try 
+		{
+			c2.setNumValue(2);
+			c2.setSuitValue("Clubs");
+		} 
+		catch (CardException e) 
+		{
+			fail();
+		}
+		//2h, 2c, 4h, 5h, 6h
+		hand.add(c1);
+		hand.add(c2);
+		hand.add(c3);
+		hand.add(c4);
+		hand.add(c5);
+		
+		assertEquals(true, e5.isPair(hand));
+		
+		try 
+		{
+			c2.setNumValue(3);
+		} 
+		catch (CardException e) 
+		{
+			fail();
+		}
+		//2h, 3c, 4h, 5h, 6h
+		assertEquals(false, e5.isPair(hand));
+		
+		hand.add(c6);
+		hand.add(c7);
+		//2h, 3c, 4h, 5h, 6h, 7h, 8h
+		assertEquals(false, e7.isPair(hand));
+		
+		try 
+		{
+			c2.setNumValue(2);
+		} 
+		catch (CardException e) 
+		{
+			fail();
+		}
+		//2h, 2c, 4h, 5h, 6h, 7h, 8h
+		assertEquals(true, e7.isPair(hand));
+	}
+	
+	@Test
+	public void testSortHand() 
+	{
+		ArrayList < Card > hand = new ArrayList < Card >();
+		
+		hand.add(c4);
+		hand.add(c1);
+		hand.add(c3);
+		hand.add(c2);
+		hand.add(c5);
+		
+		e5.sortHand(hand);
+		
+		for(int i = 0; i < hand.size() - 1; i++)
+		{
+			if(hand.get(i).getNumValue() > hand.get(i + 1).getNumValue())
+			{
+				fail();
+			}
+		}
+		
+		hand.add(c7);
+		hand.add(c6);
+		
+		e7.sortHand(hand);
+		
+		for(int i = 0; i < hand.size() - 1; i++)
+		{
+			if(hand.get(i).getNumValue() > hand.get(i + 1).getNumValue())
+			{
+				fail();
+			}
+		}
+	}
+	
+	@Test
+	public void testPairCounter() 
+	{
+		ArrayList < Card > hand = new ArrayList < Card >();
+		
+		//2h, 3h, 4h, 5h, 6h
+		hand.add(c1);
+		hand.add(c2);
+		hand.add(c3);
+		hand.add(c4);
+		hand.add(c5);
+		
+		assertEquals(0, e5.pairCounter(hand));
+		
+		try 
+		{
+			c2.setNumValue(2);
+			c2.setSuitValue("Clubs");
+		} 
+		catch (CardException e) 
+		{
+			fail();
+		}
+		
+		//2h, 2c, 4h, 5h, 6h
+		assertEquals(1, e5.pairCounter(hand));
+		
+		try 
+		{
+			c3.setNumValue(2);
+			c3.setSuitValue("Diamonds");
+		} 
+		catch (CardException e) 
+		{
+			fail();
+		}
+		
+		//2h, 2c, 2d, 5h, 6h
+		assertEquals(1, e5.pairCounter(hand));
+		
+		try 
+		{
+			c4.setNumValue(2);
+			c4.setSuitValue("Spades");
+		} 
+		catch (CardException e) 
+		{
+			fail();
+		}
+		
+		//2h, 2c, 2d, 2s, 6h
+		assertEquals(2, e5.pairCounter(hand));
+		
+		hand.add(c6);
+		hand.add(c7);
+		//2h, 2c, 2d, 2s, 6h, 7h, 8h
+		assertEquals(2, e7.pairCounter(hand));
+		
+		try 
+		{
+			c3.setNumValue(4);
+			c3.setSuitValue("Hearts");
+		} 
+		catch (CardException e) 
+		{
+			fail();
+		}
+		//2h, 2c, 4h, 2s, 6h, 7h, 8h
+		assertEquals(1, e7.pairCounter(hand));
+		
+		try 
+		{
+			c2.setNumValue(10);
+			c1.setNumValue(5);
+		} 
+		catch (CardException e) 
+		{
+			fail();
+		}
+		//5h, 10c, 4h, 2s, 6h, 7h, 8h
+		assertEquals(0, e7.pairCounter(hand));
+	}
+	
+	@Test
+	public void testNumericValueCounter() 
+	{
+		
+	}
+	
+	@Test
+	public void testFlushFinder() 
+	{
+		ArrayList < Card > hand = new ArrayList < Card >();
+		//2h, 2h, 4h, 5h, 6h
+		hand.add(c4);
+		hand.add(c1);
+		hand.add(c3);
+		hand.add(c2);
+		hand.add(c5);
+		
+		ArrayList < Card > flush = new ArrayList < Card >();
+		
+		e5.flushFinder(flush, hand);
+		assertEquals(5, flush.size());
+		for(int i = 0; i < flush.size() - 1; i++)
+		{
+			if(!flush.get(i).getSuitValue().equals(flush.get(i + 1).getSuitValue()))
+			{
+				fail();
+			}
+		}
+		
+		ArrayList < Card > flush2 = new ArrayList < Card >();
+		try 
+		{
+			c2.setSuitValue("Clubs");
+		} 
+		catch (CardException e) 
+		{
+			fail();
+		}
+		//2h, 3c, 4h, 5h, 6h
+		e5.flushFinder(flush2, hand);
+		assertEquals(0, flush2.size());
+		
+		hand.add(c6);
+		hand.add(c7);
+		//2h, 3c, 4h, 5h, 6h, 7h, 8h
+		e7.flushFinder(flush2, hand);
+		assertEquals(6, flush2.size());
+		
+		ArrayList < Card > flush3 = new ArrayList < Card >();
+		try 
+		{
+			c3.setSuitValue("Clubs");
+			c4.setSuitValue("Clubs");
+		} 
+		catch (CardException e) 
+		{
+			fail();
+		}
+		//2h, 3c, 4c, 5c, 6h, 7h, 8h
+		e7.flushFinder(flush3, hand);
+		assertEquals(0, flush3.size());
 	}
 }
